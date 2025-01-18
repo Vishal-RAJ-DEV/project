5715d9faa6eb36ac80f86ce8d051906155d7abfd
Vishal-RAJ-DEV <user.vishalraj140706@gmail.com>
2025-01-18 22:45:21 +0530

    Stashing changes in portflio

---
+++ javascript/dyanmic wesite/script.js
@@ -0,0 +1,13 @@
+// function createcard(title cName,views, monthOLD,duration){
+
+// }
+
+// createcard("introduction to Backend  | sigma web dev video #2",
+//     "codeWithHarry", 250000,77, ,
+//  )
+
+let button= document.getElementById('btn');
+
+button.addEventListener('click',()=>{
+    document.querySelector(".box").innerHTML=
+})
