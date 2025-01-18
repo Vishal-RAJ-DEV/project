2e5f4fc370f85ca5a6616564e087446298b645e1
Vishal-RAJ-DEV <user.vishalraj140706@gmail.com>
2025-01-18 22:58:07 +0530

    initial commit

---
+++ PORTFILO/script.js
@@ -0,0 +1,30 @@
+const navMenu=document.getElementById('nav-menu'),
+navToggle=document.getElementById('nav-toggle'),
+navClose=document.getElementById('nav-closed')
+
+if(navToggle){
+    navToggle.addEventListener('click',() => {
+        navMenu.classList.add('show-menu')
+    })
+}
+if(navClose){
+    navClose.addEventListener('click',() =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink=document.querySelectorAll('.nav__link')
+const linkAction =() =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+const blurHeader= () =>{
+     const header = document.getElementById('header')
+    // this will add a blur effect to the header when the user scrolls down more than 50 viewport
+     this.scrollY >= 50 ? header.classList.add('blur-header')
+                        :header.classList.remove('blur-header')
+}
+window.addEventListener('scroll',blurHeader)
